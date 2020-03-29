ded517f948a502cd49f30915e8ac1b7f64ca2308
Ilya Dotsenko <vikontart@gmail.com>
2020-03-29 23:40:06 +0300

    init

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Publisher.Views;
+using System;
+using System.Windows;
+using Publisher.ViewModels;
+
+namespace Publisher
+{
+    public class MainWindowViewModel
+    {
+        public MainWindow MainWindow { get; set; }
+
+        public SelectProjectsView SelectProjectsView { get; set; }
+
+        public MainWindowViewModel()
+        {
+            InitializeViews();
+            InitializeNavigationCommands();
+        }
+
+        private void InitializeViews()
+        {
+            SelectProjectsView = new SelectProjectsView { DataContext = new SelectProjectViewModel(@"E:\net-projects\job\evj\back") };
+        }
+
+        private void InitializeNavigationCommands()
+        {
+            // TODO
+        }
+
+        public void InitMainWindow(MainWindow mainWindow)
+        {
+            MainWindow = mainWindow;
+            MainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            MainWindow.Show();
+            MainWindow.FrameBody.NavigationService.Navigate(SelectProjectsView);
+        }
+    }
+}
---
+++ ViewModels/SelectProjectViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Input;
+
+namespace Publisher.ViewModels
+{
+    public class SelectProjectViewModel
+    {
+        private readonly string pathToSln;
+
+        public List<PublishProject> PublishProjects { get; set; }
+
+        public SelectProjectViewModel(string pathToSln)
+        {
+            this.pathToSln = pathToSln;
+            PublishProjects = new List<PublishProject>();
+
+            GetAllProjects();
+        }
+
+        public ICommand NextCommand => new RelayCommand(NextCommandExecuted);
+
+        private void NextCommandExecuted(object obj)
+        {
+            Console.WriteLine();
+        }
+
+        private void GetAllProjects()
+        {
+            foreach (var directory in Directory.GetDirectories(pathToSln))
+            {
+                var directoryInfo = new DirectoryInfo(Path.Combine(pathToSln, directory));
+                var files = directoryInfo.GetFiles("*.csproj");
+
+                if (files.Length <= 0) continue;
+
+                var fileName = files[0].Name;
+                PublishProjects.Add(new PublishProject
+                {
+                    IsSelected = false,
+                    Name = fileName.Remove(fileName.Length - 7),
+                    Path = directory
+                });
+            }
+        }
+    }
+
+    public class PublishProject
+    {
+        public bool IsSelected { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+    }
+}
